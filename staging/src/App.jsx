a509441aa04d1b382269aaf41b320df9075cdbe2
Zhiheng Chen <czhdawang@163.com>
2023-10-23 15:48:36 +1100

    finished main frame of github search project.

---
+++ staging/src/App.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import Search from './components/Search'
+import List from './components/List'
+
+export default class App extends Component {
+    render() {
+        <div className='container'>
+            <Search/>
+            <List/>
+        </div>
+    }
+}
